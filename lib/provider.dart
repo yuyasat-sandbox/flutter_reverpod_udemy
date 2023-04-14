b6a2356e04e63fc537922b1d6ed760b67ad6395c
yuyasat <takiyour@gmail.com>
2023-04-14 15:06:15 +0900

    titleProvider, messageProvider

---
+++ lib/provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final titleProvider = Provider<String>((ref) {
+  return 'Riverpod Demo Home Page 2';
+});
+
+final messageProvider =
+    Provider<String>((ref) => 'You have pushed the button this many times2:');
