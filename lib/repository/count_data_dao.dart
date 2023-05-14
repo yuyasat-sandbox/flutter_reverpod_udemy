8cc75ccdea99a61a654b1f496ecee7fefd55cacb
yuyasat <takiyour@gmail.com>
2023-05-14 16:24:15 +0900

    connect to cloudfirestore

---
+++ lib/repository/count_data_dao.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../data/count_data_fs.dart';
+
+class CountDataDao {
+  final CollectionReference _collection =
+      FirebaseFirestore.instance.collection('collection_count');
+
+  void saveCountData(CountDataFs countData) {
+    _collection.add(countData.toJson());
+  }
+}
